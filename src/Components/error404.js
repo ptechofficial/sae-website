d4b899e88e4f7cb4b422a6ec549c13a41b9dde91
ptechofficial <64535754+ptechofficial@users.noreply.github.com>
2021-05-18 14:22:03 +0530

    fixed error 404 bug

--- src/Components/error404.js
+++ src/Components/error404.js
@@ -6,14 +6,13 @@ const Error404 = () => {
     <section className='page_404'>
       <div className='container'>
         <div className='row'>
-          <div className='col-sm-12'>
-            <div className='col-sm-10 col-sm-offset-1 text-center'>
-              <div className='four_zero_four_bg'>
-                <h1 className='text-center'>404</h1>
-              </div>
+          <div>
+            <div className='text-center'>
+              <h1>404</h1>
+              <div className='four_zero_four_bg'></div>
 
               <div className='contant_box_404'>
-                <h3 className='h2'>Look like you're lost</h3>
+                <h3>Look like you're lost</h3>
 
                 <p>the page you are looking for not avaible!</p>
 
