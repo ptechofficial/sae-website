0b71c71df6788a19214c54ccc534e24c30a8f98f
ptechofficial <64535754+ptechofficial@users.noreply.github.com>
2021-05-20 17:04:26 +0530

    added images and added professors section

---
+++ src/Components/Leads.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import '../Styling/Leads.css';
+
+const Leads = () => {
+  return (
+    <>
+      <section className='testing'>
+        <section className='prof-inch'>
+          <div className='title-profile'>PROFESSOR INCHARGE</div>
+          <div className='person-profile'>
+            <span className='image-prof'></span>
+            <span className='name-prof'>Prakarsh Gupta</span>
+            <span className='desg-prof'>Designation</span>
+          </div>
+          <div className='person-profile'>
+            <span className='image-prof'></span>
+            <span className='name-prof'>Prakarsh Gupta</span>
+            <span className='desg-prof'>Designation</span>
+          </div>
+          <div className='person-profile'>
+            <span className='image-prof'></span>
+            <span className='name-prof'>Prakarsh Gupta</span>
+            <span className='desg-prof'>Designation</span>
+          </div>
+          <div className='title-profile'>MANAGER</div>
+          <div className='person-profile'>
+            <span className='image-prof'></span>
+            <span className='name-prof'>Prakarsh Gupta</span>
+            <span className='desg-prof'>Designation</span>
+          </div>
+          <div className='person-profile'>
+            <span className='image-prof'></span>
+            <span className='name-prof'>Prakarsh Gupta</span>
+            <span className='desg-prof'>Designation</span>
+          </div>
+          <div className='person-profile'>
+            <span className='image-prof'></span>
+            <span className='name-prof'>Prakarsh Gupta</span>
+            <span className='desg-prof'>Designation</span>
+          </div>
+        </section>
+      </section>
+    </>
+  );
+};
+
+export default Leads;
--- src/Home.js
+++ src/Home.js
@@ -2,16 +2,21 @@ import React from 'react';
 import './Home.css';
 import { Link } from 'react-router-dom';
 import DataHome from './data-object-file/home-data';
+
+import Overlay from './Components/Overlay';
 import Landing from './Components/Landing';
 import HomeAboutUs from './Components/HomeAboutUs';
+import Leads from './Components/Leads';
 import Footer from './Components/Footer';
 
 import Teams from './Components/Teams';
 const Home = () => {
   return (
     <>
+      <Overlay socialColor='black' />
       <Landing landing={DataHome.landing} teamName='sae' />
       <HomeAboutUs itemColor={'black'} teamData={DataHome} />
+      <Leads />
       <Footer
         accentColor1={DataHome.accentColor}
         accentColor2={DataHome.accentColor_alt}
